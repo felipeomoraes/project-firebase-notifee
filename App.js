aea40fad6b059387002133b0197ccac9b3dd352c
Felipe Moraes <felipe.omoraes@outlook.com>
2022-02-06 13:44:46 -0300

    Instalado e configurado Notifee

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+import notifee from '@notifee/react-native';
 
 export default function App() {
+
+  async function onDisplayNotification() {
+    // Create a channel
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    });
+
+    // Display a notification
+    await notifee.displayNotification({
+      title: 'Notification Title',
+      body: 'Main body content of the notification',
+      android: {
+        channelId,
+        smallIcon: 'ic_launcher', // optional, defaults to 'ic_launcher'.
+      },
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Teste Notifee</Text>
+      <TouchableOpacity style={styles.btn} onPress={() => onDisplayNotification()}>
+        <Text style={styles.text}>Clique aqui</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -18,4 +42,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  btn: {
+    alignItems: 'center',
+    justifyContent:'center',
+    backgroundColor: "blue",
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderRadius: 5
+  },
+  text: {
+    fontSize: 32,
+    color: '#fff'
+  }
 });
